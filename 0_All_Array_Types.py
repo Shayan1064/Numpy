f7811cd5690c64a76cee2ebe13ca44fd71348c13
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-14 23:51:50 +0500

    All type of Array

---
+++ 0_All_Array_Types.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# 1D Array
+
+arr=np.array([1,2,3])
+print(arr)
+shape=arr.shape
+di=arr.ndim
+print("The size of array: ",shape)
+print("The dimension of array: ",di)
+
+# 2D Array
+
+# arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(arr)
+# shape=arr.shape
+# di=arr.ndim
+# print("The size of array: ",shape)
+# print("The dimension of array: ",di)
+
+#  3D Array
+
+# arr=np.array([
+#     [[1,2,3],[4,5,6],[7,8,9]]
+#     ])
+# print(arr)
+# shape=arr.shape
+# di=arr.ndim
+# print("The size of array: ",shape)
+# print("The dimension of array: ",di)
+
+
+# 4D Array
+
+# arr=np.array([
+#     [[[1,2,3],[4,5,6],[7,8,9]]]
+#     ])
+# print(arr)
+# shape=arr.shape
+# di=arr.ndim
+# print("The size of array: ",shape)
+# print("The dimension of array: ",di)
+
+
