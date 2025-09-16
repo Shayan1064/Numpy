5d5816df3de7c14c39788addb3c696ce0806b57b
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-16 22:30:37 +0500

    What is Reshaping

--- 13_Stacking.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-arr1=np.arange(6).reshape(2,3)
-arr2=np.arange(7,13).reshape(2,3)
-
-arr3=np.hstack((arr1,arr2))
-print("The Hstake is: ",arr3)
-
-arr4=np.vstack((arr1,arr2))
-print("The Vstake is: ",arr4)
